102c64c1a90a4bf4513493144644c130c527d50a
Tingting Zhao <zhaott0416@gmail.com>
2019-05-09 23:01:36 -0400

    add argument

--- DP.py
+++ DP.py
@@ -40,6 +40,7 @@ parser.add_argument('-useNewPretrained', action='store_true',  dest='useNewPretr
 parser.add_argument('-useUnsupervised', action='store_true', help='if true, use the original latent representation from the author')
 parser.add_argument('-learningRate', action='store', type=float, dest='lr', default=0.01, help='the learning rate in adam_nn')
 parser.add_argument('-datasetPath', action='store', type=str, dest='datasetPath', help='the path for new cluster dataset')
+parser.add_argument('-initModelPath', action='store', type=str, help='the path for the initial model of DP')
 
 
 results = parser.parse_args()
