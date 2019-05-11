578111c9a93f154d1a19f43c47860bf708cb7c12
Zifeng Wang <zifengwang@ece.neu.edu>
2019-05-11 02:19:34 -0400

    run_dp.sh

--- run_dp.sh
+++ run_dp.sh
@@ -3,7 +3,7 @@ for i in {1..5}
 do
     python VaDE_dp.py \
         -bnpyPath "/home/zifeng/Research/bnpy" \
-        -outputPath "/home/zifeng/Research/DPVAE/mnist_results_new_recon2" \
+        -outputPath "/home/zifeng/Research/DPVAE/mnist_results_new_recon4" \
         -rootPath "/home/zifeng/Research/DPVAE" \
         -epoch 10 \
         -Kmax 50 \
@@ -15,7 +15,8 @@ do
         -rep $i \
         -sf 0.1 \
         -learningRate 2e-4 \
-        -batchsize 5000
+        -batchsize 500 \
+        -nBatch 1
 done
 
 
